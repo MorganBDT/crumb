4b96b0a475f259d0708709908396e692a68e03cc
MorganBDT <morgan_talbot@alumni.brown.edu>
2023-09-15 15:52:13 -0400

    crumb pretrain with direct loss only (essentially vanilla pretraining)

--- scripts/ablation/crumb_pretrain_imagenet_vanilla.sh
+++ scripts/ablation/crumb_pretrain_imagenet_vanilla.sh
@@ -1,9 +1,16 @@
+# Param #1: dataset name, e.g. imagenet
+# Param #2: GPU ID. Default is 0
+# Usage example: ./scripts/Crumb.sh imagenet 0  (runs the model on gpu 0)
+# Usage example: ./scripts/Crumb.sh imagenet 0 0.001 256 8 0 /media/data/alt_imagenet_dir
 DATASET="${1:-"imagenet"}"
 GPU="${2:-0}"
 RUN="${3:-0}"
 lr=${4:-0.001}
+N_MEMBLOCKS=${5:-256}
+MEMBLOCK_LENGTH=${6:-8}
+memory_size=${7:-0} # Meaningless in pretraining (all replay operations skipped)
 RUNS_STR=$(echo "${RUN}" | sed 's/ /-/g') # If "RUN" is formatted like "0 1 2", replace spaces with dashes
-OUTDIR="${DATASET}_pretrain_vanilla_run${RUNS_STR}"
+OUTDIR="${DATASET}_pretrain_direct_loss_only_run${RUNS_STR}"
 if [ "$DATASET" = "core50" ]; then
     DDATAROOT="/media/mengmi/KLAB15/Mengmi/proj_CL_NTM/data/core50"
 elif [ "$DATASET" = "toybox" ]; then
@@ -21,6 +28,6 @@ fi
 
 DATAROOT=${8:-${DDATAROOT}}
 
-mkdir -p ${OUTDIR}/iid/NormalNN_SqueezeNet_offline
+mkdir -p ${OUTDIR}/iid/Crumb_SqueezeNet_offline
 
-python -u experiment.py --offline --scenario iid --save_model --acc_topk 1 5 --replay_times 0 --dataset $DATASET --dataroot $DATAROOT --specific_runs $RUN --n_epoch 1 --n_epoch_first_task 10 --lr ${lr} --freeze_feature_extract --model_type squeezenet --model_name SqueezeNet --pretrained --agent_type crumb --agent_name Crumb  --gpuid $GPU --momentum 0.9 --weight_decay 0.0001 --batch_size 128 --n_workers 8 --output_dir ${OUTDIR} | tee ${OUTDIR}/iid/NormalNN_SqueezeNet_offline/log.log
+python -u experiment_aug.py --offline --storage_type image --scenario iid --save_model --acc_topk 1 5 --replay_times 0 --dataset $DATASET --dataroot $DATAROOT --specific_runs $RUN --n_epoch 1 --n_epoch_first_task 10 --lr ${lr} --n_memblocks ${N_MEMBLOCKS} --memblock_length ${MEMBLOCK_LENGTH} --memory_size ${memory_size} --replay_coef 0 --freeze_feature_extract --model_type squeezenet --model_name SqueezeNet --pretrained --agent_type crumb --agent_name Crumb  --gpuid $GPU --momentum 0.9 --weight_decay 0.0001 --batch_size 128 --n_workers 8 --output_dir ${OUTDIR} | tee ${OUTDIR}/iid/Crumb_SqueezeNet_offline/log.log
