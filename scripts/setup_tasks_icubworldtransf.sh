75aeff05cb1b709ee703f9020c7c9812793c0eca
MorganBDT <morgan_talbot@alumni.brown.edu>
2023-09-23 13:32:26 -0400

    setup tasks for icub

---
+++ scripts/setup_tasks_icubworldtransf.sh
@@ -0,0 +1,7 @@
+# icubworldtransf task setup
+
+python dataloaders/task_setup.py --dataset icubworldtransf --scenario class_iid --test_sess 0        --offline
+python dataloaders/task_setup.py --dataset icubworldtransf --scenario class_instance --test_sess 0   --offline
+
+python dataloaders/task_setup.py --dataset icubworldtransf --scenario class_iid --test_sess 0
+python dataloaders/task_setup.py --dataset icubworldtransf --scenario class_instance --test_sess 0
