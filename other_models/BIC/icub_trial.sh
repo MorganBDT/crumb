0f9a1fea2c3e53f2fe0e4113fe2bce57a29fe832
MorganBDT <morgan_talbot@alumni.brown.edu>
2023-10-02 09:54:22 -0400

    Batch size

--- other_models/BIC/icub_trial.sh
+++ other_models/BIC/icub_trial.sh
@@ -1,10 +1,10 @@
 MY_PYTHON="python"
 
 echo "Training class instance on icubworldtransf"
-$MY_PYTHON main.py  --dataset icubworldtransf --epoch 15 --lr 0.0001  --total_cls 20 --numrun 10 --paradigm "class_instance"
+$MY_PYTHON main.py  --dataset icubworldtransf --epoch 15 --batch_size 21 --lr 0.0001  --total_cls 20 --numrun 10 --paradigm "class_instance"
 
 echo "Training class iid on icubworldtransf"
-$MY_PYTHON main.py  --dataset icubworldtransf --epoch 15 --lr 0.0001  --total_cls 20 --numrun 10 --paradigm "class_iid"
+$MY_PYTHON main.py  --dataset icubworldtransf --epoch 15 --batch_size 21 --lr 0.0001  --total_cls 20 --numrun 10 --paradigm "class_iid"
 
 
 
--- other_models/BIC/ilab2mlight+core50_trial.sh
+++ other_models/BIC/ilab2mlight+core50_trial.sh
@@ -1,10 +1,10 @@
 MY_PYTHON="python"
 
 echo "Training class instance on ilab2mlight+core50"
-$MY_PYTHON main.py  --dataset ilab2mlight+core50 --epoch 15 --lr 0.0001  --total_cls 24 --numrun 10 --paradigm "class_instance"
+$MY_PYTHON main.py  --dataset ilab2mlight+core50 --epoch 15 --batch_size 21 --lr 0.0001  --total_cls 24 --numrun 10 --paradigm "class_instance"
 
 echo "Training class iid on ilab2mlight+core50"
-$MY_PYTHON main.py  --dataset ilab2mlight+core50 --epoch 15 --lr 0.0001  --total_cls 24 --numrun 10 --paradigm "class_iid"
+$MY_PYTHON main.py  --dataset ilab2mlight+core50 --epoch 15 --batch_size 21 --lr 0.0001  --total_cls 24 --numrun 10 --paradigm "class_iid"
 
 
 
