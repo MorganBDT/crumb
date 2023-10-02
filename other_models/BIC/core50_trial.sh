a01f6d71acb08b9d4fdd12db06ea6593d2d0b902
MorganBDT <morgan_talbot@alumni.brown.edu>
2023-10-02 10:26:08 -0400

    buffer sizes

--- other_models/BIC/core50_trial.sh
+++ other_models/BIC/core50_trial.sh
@@ -1,10 +1,10 @@
 MY_PYTHON="python"
 
 echo "Training class instance on CORe50"
-$MY_PYTHON main.py  --dataset core50 --epoch 15 --lr 0.0001  --total_cls 10 --numrun 10 --paradigm "class_instance"
+$MY_PYTHON main.py  --dataset core50 --max_size 15 --epoch 15 --batch_size 21 --lr 0.0001  --total_cls 10 --numrun 10 --paradigm "class_instance"
 
 echo "Training class iid on CORe50"
-$MY_PYTHON main.py  --dataset core50 --epoch 15 --lr 0.0001  --total_cls 10 --numrun 10 --paradigm "class_iid"
+$MY_PYTHON main.py  --dataset core50 --max_size 15 --epoch 15 --batch_size 21 --lr 0.0001  --total_cls 10 --numrun 10 --paradigm "class_iid"
 
 
 
