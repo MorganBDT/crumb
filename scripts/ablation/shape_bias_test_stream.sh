40e676d9d853f42ce0b27e5f47641b708e20ca74
MorganBDT <morgan_talbot@alumni.brown.edu>
2023-10-15 17:10:09 -0400

    style transfer for imagenet stream

--- scripts/ablation/shape_bias_test_stream.sh
+++ scripts/ablation/shape_bias_test_stream.sh
@@ -11,6 +11,9 @@ cut_layer=${6:-12}
 model_name=${7:-"SqueezeNet"}
 OUTDIR="./ablation_study/${DATASET}_${ABLATION_NAME}"
 
+
+INCLUDE_STYLE_TRANSFER=""
+ACC_TOPK=""
 if [ "$DATASET" = "core50" ]; then
     #DATAROOT="./data/core50"
     DATAROOT="/media/data/Datasets/Core50"
@@ -37,15 +40,21 @@ elif [ "$DATASET" = "imagenet" ]; then
     DATAROOT="/media/KLAB37/datasets/ImageNet2012"
     NCLASS=1000
     SCENARIOS=("class_iid")
+    INCLUDE_STYLE_TRANSFER="--include_style_transfer"
+    ACC_TOPK="--acc_topk 1 5"
 else
     echo "Invalid dataset name!"
     exit
 fi
 
+STYLE_TRANSFER_DATAROOT="/media/KLAB37/datasets/imagenet-styletransfer-v2"
+
+
+
 RUNS=(0 1 2 3 4)
 for scenario in "${SCENARIOS[@]}"; do
   for run in "${RUNS[@]}"; do
     weights_path=$OUTDIR/$scenario/Crumb_"$model_name"/runs-"$run"/CRUMB_run"$run"
-    python -u shape_bias_test.py --scenario $scenario --visualize --specific_runs $run --n_memblocks "$N_MEMBLOCKS" --memblock_length "$MEMBLOCK_LENGTH" --crumb_cut_layer $cut_layer --model_type squeezenet --model_name "$model_name" --pretrained --agent_type crumb --agent_name Crumb --batch_size 100 --n_workers 0 --pretrained_weights --model_weights "$weights_path" --memory_weights "$weights_path" --pretrained_dataset_no_of_classes "$NCLASS" --gpuid "$GPU" --dataset "$DATASET" --dataroot "$DATAROOT"  --output_dir "$OUTDIR" | tee "$OUTDIR"/"$scenario"/Crumb_"$model_name"/runs-"$run"/shape_bias_log.log
+    python -u shape_bias_test.py --scenario $scenario --visualize --imagenet_styletransfer_dataroot $STYLE_TRANSFER_DATAROOT $INCLUDE_STYLE_TRANSFER $ACC_TOPK --specific_runs $run --n_memblocks "$N_MEMBLOCKS" --memblock_length "$MEMBLOCK_LENGTH" --crumb_cut_layer $cut_layer --model_type squeezenet --model_name "$model_name" --pretrained --agent_type crumb --agent_name Crumb --batch_size 100 --n_workers 0 --pretrained_weights --model_weights "$weights_path" --memory_weights "$weights_path" --pretrained_dataset_no_of_classes "$NCLASS" --gpuid "$GPU" --dataset "$DATASET" --dataroot "$DATAROOT"  --output_dir "$OUTDIR" | tee "$OUTDIR"/"$scenario"/Crumb_"$model_name"/runs-"$run"/shape_bias_log.log
   done
 done
