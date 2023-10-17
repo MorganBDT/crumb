c0937d60136a0b4897b3849d84063e3164ef6abc
MorganBDT <morgan_talbot@alumni.brown.edu>
2023-10-17 17:13:08 -0400

    stats

--- network_analysis_stats.py
+++ network_analysis_stats.py
@@ -8,39 +8,61 @@ else:
     dataset = "core50"
 runs = 5
 
+control_scenario = "unablated"
+
 if dataset == "core50":
     ablations = (
         "unablated",
-        "cutlayer_3",
+        # # "cutlayer_3",
+        # # "pretrained_image_replay",
+        # # "stock_pretrain_image_replay",
+        # # "no_pretrain",
+        # # "cifar100_pretrain_4epochs",
+        # # "MobileNet_normal_init",
+        # # "stdnml",
+        # # "uniform",
+        # # "distmatch_dense",
+        # # "no_replay",
+        # # "half_mem_cap",
+        # # "quarter_mem_cap",
+        # # "direct_loss_in_streaming",
+        # # "no_direct_loss_in_pt",
+        # # "only_direct_loss_in_streaming",
+        # # "2_augmem_rows",
+        # # "4_augmem_rows",
+        # # "8_augmem_rows",
+        # # "16_augmem_rows",
+        # # "32_augmem_rows",
+        # # "64_augmem_rows",
+        # # "128_augmem_rows",
+        # # "512_augmem_rows",
+        # # "4_augmem_feat",
+        # # "16_augmem_feat",
+        # # "32_augmem_feat",
+        # # "16_augmem_feat_buffer_size_400",
+        # # "32_augmem_feat_buffer_size_800",
+        # # "freeze_memory",
+        "pt_weights_only",
+        "pt_memblocks_only",
         "pretrained_image_replay",
-        "stock_pretrain_image_replay",
-        "no_pretrain",
-        "cifar100_pretrain_4epochs",
-        "MobileNet_normal_init",
-        "stdnml",
-        "uniform",
-        "distmatch_dense",
-        "no_replay",
-        "half_mem_cap",
-        "quarter_mem_cap",
-        "direct_loss_in_streaming",
-        "no_direct_loss_in_pt",
-        "only_direct_loss_in_streaming",
-        "2_augmem_rows",
-        "4_augmem_rows",
-        "8_augmem_rows",
-        "16_augmem_rows",
-        "32_augmem_rows",
-        "64_augmem_rows",
-        "128_augmem_rows",
-        "512_augmem_rows",
-        "4_augmem_feat",
-        "16_augmem_feat",
-        "32_augmem_feat",
-        "16_augmem_feat_buffer_size_400",
-        "32_augmem_feat_buffer_size_800",
-        "freeze_memory"
+        "merec_instead_of_crumb",
+        "memcap_400",
+        "memcap_800",
+        "memcap_1600",
+        "memcap_3200",
+        "memcap_6400",
+        "stock_pretrain_image_replay_memcap_400",
+        "stock_pretrain_image_replay_memcap_800",
+        "stock_pretrain_image_replay_memcap_1600",
+        "stock_pretrain_image_replay_memcap_3200",
+        "stock_pretrain_image_replay_memcap_6400",
     )
+    # memcap = 6400
+    # control_scenario = "memcap_" + str(memcap)
+    # ablations = (
+    #     "memcap_" + str(memcap),
+    #     "stock_pretrain_image_replay_memcap_" + str(memcap),
+    # )
 else:
     ablations = (
         "unablated",
@@ -56,7 +78,7 @@ else:
 
 all_dfs = {scenario: {} for scenario in scenarios}
 
-print("Paired-sample t-test results (all paired with unablated): ")
+print("Paired-sample t-test results (all paired with" + control_scenario + "): ")
 
 for ablation in ablations:
     if "MobileNet" in ablation:
@@ -67,15 +89,20 @@ for ablation in ablations:
     for scenario in scenarios:
         run_dfs = []
         for run in range(runs):
-            run_dfs.append(pd.read_csv(
-                "./ablation_study/" + dataset + "_" + ablation + "/" + scenario + "/Crumb_" + model
-                + "/runs-" + str(run) + "/batchwise_accs_run" + str(run) + ".csv"))
+            try:
+                run_dfs.append(pd.read_csv(
+                    "./ablation_study/" + dataset + "_" + ablation + "/" + scenario + "/Crumb_" + model
+                    + "/runs-" + str(run) + "/batchwise_accs_run" + str(run) + ".csv"))
+            except FileNotFoundError:
+                run_dfs.append(pd.read_csv(
+                    "./ablation_study/" + dataset + "_" + ablation + "/" + scenario + "/AugMem_" + model
+                    + "/runs-" + str(run) + "/batchwise_accs_run" + str(run) + ".csv"))
 
         all_dfs[scenario][ablation] = pd.concat(run_dfs)
 
         if not ablation == "unablated":
             print(ablation + ", " + scenario + ":")
-            print(stats.ttest_rel(all_dfs[scenario]["unablated"]["accuracy"], all_dfs[scenario][ablation]["accuracy"]))
+            print(stats.ttest_rel(all_dfs[scenario][control_scenario]["accuracy"], all_dfs[scenario][ablation]["accuracy"]))
 
 
 
