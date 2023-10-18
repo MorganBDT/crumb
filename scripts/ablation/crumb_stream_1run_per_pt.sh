1cd44d561c84d80c7c97e07c16fdbf7d16469dd7
MorganBDT <morgan_talbot@alumni.brown.edu>
2023-10-18 10:37:49 -0400

    optim

--- scripts/ablation/crumb_stream_1run_per_pt.sh
+++ scripts/ablation/crumb_stream_1run_per_pt.sh
@@ -29,11 +29,21 @@ elif [ "$DATASET" = "ilab2mlight" ]; then
     MEMORY_SIZE=200
     BATCH_SIZE=21
     N_EPOCH_FIRST_TASK=10
+elif [ "$DATASET" = "ilab2mlight+core50" ]; then
+    DATAROOT="/media/KLAB37/datasets/"
+    MEMORY_SIZE=400
+    BATCH_SIZE=21
+    N_EPOCH_FIRST_TASK=10
+elif [ "$DATASET" = "icubworldtransf" ]; then
+    DATAROOT="/media/KLAB37/datasets/icubworldtransf_sparse"
+    MEMORY_SIZE=400
+    BATCH_SIZE=21
+    N_EPOCH_FIRST_TASK=10
 elif [ "$DATASET" = "cifar100" ]; then
     #DATAROOT="./data/cifar100"
     DATAROOT="/media/KLAB37/datasets/cifar100"
     MEMORY_SIZE=2000
-    BATCH_SIZE=21
+    BATCH_SIZE=128
     N_EPOCH_FIRST_TASK=10
 elif [ "$DATASET" = "imagenet" ]; then
     #DATAROOT="/n/groups/kreiman/shared_data/Imagenet2012"
@@ -52,6 +62,6 @@ for RUN in "${RUNS[@]}"; do
     mkdir -p "$OUTDIR"/class_instance/Crumb_SqueezeNet/runs-"$RUN"
     weights_path=./"$PRETRAIN_DIR"/iid/Crumb_SqueezeNet_offline/runs-"$RUN"/CRUMB_run"$RUN"
 
-    python -u experiment_aug.py --scenario class_iid      --save_model_every_epoch --specific_runs $RUN --n_epoch_first_task $N_EPOCH_FIRST_TASK --n_epoch 1 --replay_times 1 --replay_coef 5 --n_memblocks "$N_MEMBLOCKS" --memblock_length "$MEMBLOCK_LENGTH" --pretrained_dataset_no_of_classes "$PRETRAIN_N_CLASSES" --freeze_feature_extract --model_type squeezenet --model_name SqueezeNet --pretrained --agent_type crumb --agent_name Crumb --momentum 0.9 --weight_decay 0.0001 --batch_size $BATCH_SIZE --n_workers 8 --pretrained_weights --model_weights "$weights_path" --memory_weights "$weights_path" --lr "$LR" --memory_size "$MEMORY_SIZE" --gpuid "$GPU" --dataset "$DATASET" --dataroot "$DATAROOT"  --output_dir "$OUTDIR" | tee "$OUTDIR"/class_iid/Crumb_SqueezeNet/runs-"$RUN"/log.log
-    python -u experiment_aug.py --scenario class_instance --save_model_every_epoch --specific_runs $RUN --n_epoch_first_task $N_EPOCH_FIRST_TASK --n_epoch 1 --replay_times 1 --replay_coef 5 --n_memblocks "$N_MEMBLOCKS" --memblock_length "$MEMBLOCK_LENGTH" --pretrained_dataset_no_of_classes "$PRETRAIN_N_CLASSES" --freeze_feature_extract --model_type squeezenet --model_name SqueezeNet --pretrained --agent_type crumb --agent_name Crumb --momentum 0.9 --weight_decay 0.0001 --batch_size $BATCH_SIZE --n_workers 8 --pretrained_weights --model_weights "$weights_path" --memory_weights "$weights_path" --lr "$LR" --memory_size "$MEMORY_SIZE" --gpuid "$GPU" --dataset "$DATASET" --dataroot "$DATAROOT"  --output_dir "$OUTDIR" | tee "$OUTDIR"/class_instance/Crumb_SqueezeNet/runs-"$RUN"/log.log
+    python -u experiment_aug.py --scenario class_iid      --save_model --specific_runs $RUN --n_epoch_first_task $N_EPOCH_FIRST_TASK --n_epoch 1 --replay_times 1 --replay_coef 5 --n_memblocks "$N_MEMBLOCKS" --memblock_length "$MEMBLOCK_LENGTH" --pretrained_dataset_no_of_classes "$PRETRAIN_N_CLASSES" --freeze_feature_extract --model_type squeezenet --model_name SqueezeNet --pretrained --agent_type crumb --agent_name Crumb --momentum 0.9 --weight_decay 0.0001 --batch_size $BATCH_SIZE --n_workers 8 --pretrained_weights --model_weights "$weights_path" --memory_weights "$weights_path" --lr "$LR" --memory_size "$MEMORY_SIZE" --gpuid "$GPU" --dataset "$DATASET" --dataroot "$DATAROOT"  --output_dir "$OUTDIR" | tee "$OUTDIR"/class_iid/Crumb_SqueezeNet/runs-"$RUN"/log.log
+    python -u experiment_aug.py --scenario class_instance --save_model --specific_runs $RUN --n_epoch_first_task $N_EPOCH_FIRST_TASK --n_epoch 1 --replay_times 1 --replay_coef 5 --n_memblocks "$N_MEMBLOCKS" --memblock_length "$MEMBLOCK_LENGTH" --pretrained_dataset_no_of_classes "$PRETRAIN_N_CLASSES" --freeze_feature_extract --model_type squeezenet --model_name SqueezeNet --pretrained --agent_type crumb --agent_name Crumb --momentum 0.9 --weight_decay 0.0001 --batch_size $BATCH_SIZE --n_workers 8 --pretrained_weights --model_weights "$weights_path" --memory_weights "$weights_path" --lr "$LR" --memory_size "$MEMORY_SIZE" --gpuid "$GPU" --dataset "$DATASET" --dataroot "$DATAROOT"  --output_dir "$OUTDIR" | tee "$OUTDIR"/class_instance/Crumb_SqueezeNet/runs-"$RUN"/log.log
 done
