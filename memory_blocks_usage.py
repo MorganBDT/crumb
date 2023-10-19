57f21f47c790df96817785c3c0fa5bf9c0d3e4e4
MorganBDT <morgan_talbot@alumni.brown.edu>
2023-10-19 19:37:59 -0400

    tweaks

--- memory_blocks_usage.py
+++ memory_blocks_usage.py
@@ -200,7 +200,7 @@ def make_visualizations(agent, transforms, args, run, tasks, active_out_nodes, t
             ggplot(df, aes(x='memory block index', y='frequency')) +
             geom_bar(stat='identity', fill='black') +
             #geom_errorbar(aes(ymin='frequency-std_dev', ymax='frequency+std_dev'), width=0.25) +
-            geom_text(aes(x=label_positions[0]+2, y=label_frequencies[0]+2, label=label_inds.numpy()[0]), va='bottom', nudge_y=0.00001, size=12) +
+            geom_text(aes(x=label_positions[0], y=label_frequencies[0], label=label_inds.numpy()[0]), va='bottom', nudge_x=3, nudge_y=0.00001, size=12) +
             geom_segment(aes(x=label_positions[0], xend=label_positions[0], y=label_frequencies[0] + 0.000009, yend=label_frequencies[0] + 0.000001), size=1.5, color=colors[0], arrow=arrow(type='closed', angle=15, length=0.1, ends='last')) +
             geom_text(aes(x=label_positions[1], y=label_frequencies[1], label=label_inds.numpy()[1]), va='bottom', nudge_y=0.00001, size=12) +
             geom_segment(aes(x=label_positions[1], xend=label_positions[1], y=label_frequencies[1] + 0.000009, yend=label_frequencies[1] + 0.000001), size=1.5, color=colors[1], arrow=arrow(type='closed', angle=15, length=0.1, ends='last')) +
