60f39225a42fdfd051d3f82db8adcf2ecb63c3f4
MorganBDT <morgan_talbot@alumni.brown.edu>
2023-10-20 17:26:34 -0400

    better frame skip calc

--- agents/crumb.py
+++ agents/crumb.py
@@ -268,6 +268,7 @@ class Crumb(nn.Module):
         print('=====================Storing replay examples=====================')
         avgSampleNum = math.floor(self.capacity/len(self.active_out_nodes))
         img_skip = math.floor(( ((len(train_loader)-1)*self.config["batch_size"]) / self.config["memory_size"])) # * (len(self.active_out_nodes) / self.config["n_class"]))
+        print("EST IMAGES:", (len(train_loader)-1)*self.config["batch_size"])
         print("IMG_SKIP:", img_skip)
         if img_skip < 1:
             img_skip = 1
